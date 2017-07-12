c4dc1677aea08a6ced0a35934430c5c493df83fe
Edson Alves <prof.edson.alves.costa@gmail.com>
2017-07-12 16:03:49 -0300

    Linux .deb package tested.

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -6,7 +6,7 @@
 project=test
 package_major=1
 package_minor=0
-package_release=1
+package_release=2
 package_version=$package_major.$package_minor-$package_release
 package_name=$project\_$package_version
 
@@ -48,7 +48,7 @@ function gen_deb()
     mkdir -p $exec_dir
     chmod 755 $exec_dir
 
-    echo "export LD_LIBRARY_PATH=/opt/$project/lib && cd /opt/$project && ./$project" > $exec_dir/$project
+    printf "#!/bin/bash\nexport LD_LIBRARY_PATH=/var/games/$project/lib && cd /var/games/$project/ && ./$project" > $exec_dir/$project
     chmod 755 $exec_dir/$project
 
     # Debian package info dir
@@ -73,6 +73,17 @@ function gen_deb()
     chmod 644 $doc_dir/copyright
 
     gzip -9 $doc_dir/changelog.Debian
+
+    man_dir=$share_dir/man
+    mkdir -p $man_dir
+    chmod 755 $man_dir
+
+    section_dir=$man_dir/man6
+    mkdir -p $section_dir
+    chmod 755 $section_dir
+
+    cp dist/linux/debian/$project.6 $section_dir/
+    gzip -9 $section_dir/$project.6
 
     # Build the package
     fakeroot dpkg-deb --build $tmp_dir
