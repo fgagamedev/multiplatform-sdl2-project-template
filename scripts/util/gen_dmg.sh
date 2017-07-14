2df10addd89a97ddd7402013f8e10e6a310673d7
Edson Alves <edsomjr@gmail.com>
2017-07-14 18:19:00 +0000

    Completing MacOS .dmg setup.

--- scripts/util/gen_dmg.sh
+++ scripts/util/gen_dmg.sh
@@ -6,14 +6,17 @@
 # Include project metadata
 . metadata.ini
 
+PACKAGE_NAME=$EXECUTABLE_NAME
 OUTPUT_FILE=$PACKAGE_NAME.dmg
 
 function gen_dmg()
 {
-	base_dir=.dmg/"$PACKAGE_NAME"
-	mkdir -p $base_dir	
-	cp -f src/$EXECUTABLE_NAME_release $base_dir/$EXECUTABLE_NAME
-	
+	base_dir=.dmg/"$PACKAGE_NAME".app/Contents
+    macos_dir=$base_dir/MacOS
+	mkdir -p $macos_dir
+	cp -f src/$EXECUTABLE_NAME\_release $macos_dir/$EXECUTABLE_NAME
+    cp -f dist/macos/Info.plist $base_dir
+
     lib_dir=$base_dir/Frameworks
 	mkdir -p $lib_dir
 
@@ -22,11 +25,15 @@ function gen_dmg()
         cp -R lib/$extlib/macos/release/* $lib_dir/;
     done
 	
-	cd .dmg
-	rm -f $OUTPUT_FILE
-	create_dmg --volname "$GAME_TITLE Installer" --window-pos 200 120 --window-size 800 400 --app-drop-link 600 185 $OUTPUT_FILE .
-	cp *.dmg ..
-	cd ..
+    resources_dir=$base_dir/Resources
+    mkdir -p $resources_dir
+
+    # Build the .dmg
+	#cd .dmg
+	#rm -f $OUTPUT_FILE
+	#create_dmg --volname "$GAME_TITLE Installer" --window-pos 200 120 --window-size 800 400 --app-drop-link 600 185 $OUTPUT_FILE .
+	#cp *.dmg ..
+	#cd ..
 }
 
 echo "Generating "$OUTPUT_FILE "..."
