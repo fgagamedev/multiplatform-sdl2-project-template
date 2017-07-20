f2da212668fd6a689de5d2f0a167f3cf6d590b69
vagrant <vagrant@localhost.localdomain>
2017-07-20 13:39:23 +0000

    RPM build not done yet.

--- dist/linux/redhat/test.spec
+++ dist/linux/redhat/test.spec
@@ -1,12 +1,14 @@
-Name: package_name
-Version: 1
-Release: 1%{?dist}
-Summary: Short description       
+Name: %%PACKAGE_NAME%%
+Version: %%VERSION_MAJOR%%.%%VERSION_MINOR%%
+Release: %%VERSION_RELEASE%%%{?dist}
+Summary: %%GAME_DESCRIPTION%%
 
 Group: Amusements/Games
 License: GPL
 URL: package_github_page
-Source0: package_name.tar.gz
+Source0: %{name}.tar.gz
+
+BuildRoot: %{_tmppath}/%{name}-root
 
 %description
 Long package description
@@ -16,16 +18,29 @@ Long package description
 
 
 %build
-./build.sh release
+./scripts/build.sh release
 
 
 %install
-rm -rf $RPM_BUILD_ROOT
-./run.sh release
+mkdir %{buildroot}/var/games/%{name}/lib
+cp src/${name}_release %{buildroot}/var/games/%{name}/%{name}
 
+for extlib in `ls lib`;
+do
+    cp -P lib/$extlib/linux/release/* %{buildroot}/var/games/%{name}/lib;
+done
+
+mkdir %{buildroot}/var/games/%{name}/resources
+cp -r resources/*  %{buildroot}/var/games/%{name}/resources/
+
+mkdir %{buildroot}/usr/games/
+cp -P dist/linux/redhat/%{name} %{buildroot}/usr/games/
 
 %files
-%doc
+%attr(755,root,root) %{buildroot]}/usr/games/%{name}
+%attr(755,root,root) %{buildroot]}/var/games/%{name}
+%defattr(644,root,root)
+%{buildroot]}/var/games/%{name}/
 
 
 %changelog
--- scripts/util/gen_rpm.sh
+++ scripts/util/gen_rpm.sh
@@ -12,55 +12,31 @@ OUTPUT_FILE=$PACKAGE_NAME\_$PACKAGE_VERSION.rpm
 
 function gen_rpm()
 {
-    # Build dir
-    tmp_dir=/tmp/$PACKAGE_NAME\_$PACKAGE_VERSION
-    rm -rf $tmp_dir
-    mkdir -p $tmp_dir
-    cd $tmp_dir && rpmdev-setuptree
+    # RPM build dir setup
+    rpmdev-setuptree
 
-    exit 0
+    # Preparing the spec file
+    spec_file=$PACKAGE_NAME.spec
+    cp dist/linux/redhat/$spec_file ~/rpmbuild/SPECS
 
-    # Data dir: resources, scripts and executable
-    var_dir=$tmp_dir/var
-    data_dir=$var_dir/games
-    install_dir=$data_dir/$PACKAGE_NAME
-    mkdir -p $install_dir
-
-    cp src/$EXECUTABLE_NAME\_release $install_dir/$EXECUTABLE_NAME
-
-    lib_dir=$install_dir/lib
-    mkdir -p $lib_dir
-
-    for extlib in `ls lib`;
-    do
-        cp -P lib/$extlib/linux/release/* $lib_dir;
-    done
-
-    # Removing embedded libraries
-    rm $lib_dir/libjpeg*
-    rm $lib_dir/libpng*
-
-    resources_dir=$install_dir/resources
-    mkdir -p $resources_dir
-
-    cp -r resources/* $resources_dir/
+    sed -i -- 's/%%PACKAGE_NAME%%/'$PACKAGE_NAME'/' ~/rpmbuild/SPECS/$spec_file
+    sed -i -- 's/%%VERSION_MAJOR%%/'$VERSION_MAJOR'/' ~/rpmbuild/SPECS/$spec_file
+    sed -i -- 's/%%VERSION_MINOR%%/'$VERSION_MINOR'/' ~/rpmbuild/SPECS/$spec_file
+    sed -i -- 's/%%VERSION_RELEASE%%/'$VERSION_RELEASE'/' ~/rpmbuild/SPECS/$spec_file
+    sed -i -- 's/%%GAME_DESCRIPTION%%/'"$GAME_DESCRIPTION"'/' ~/rpmbuild/SPECS/$spec_file
 
     # Launcher script dir
-    usr_dir=$tmp_dir/usr
-    exec_dir=$usr_dir/games
-    mkdir -p $exec_dir
+    printf "#!/bin/bash\nexport LD_LIBRARY_PATH=/var/games/$PACKAGE_NAME/lib && cd /var/games/$PACKAGE_NAME/ && ./$EXECUTABLE_NAME\n" > dist/linux/redhat/$EXECUTABLE_NAME
 
-    printf "#!/bin/bash\nexport LD_LIBRARY_PATH=/var/games/$PACKAGE_NAME/lib && cd /var/games/$PACKAGE_NAME/ && ./$EXECUTABLE_NAME" > $exec_dir/$EXECUTABLE_NAME
+    # Preparing the source package
+    tar --transform 's,^,'$PACKAGE_NAME-$VERSION_MAJOR.$VERSION_MINOR'/,' -cvzf ${PACKAGE_NAME}.tar.gz .
+    cp ${PACKAGE_NAME}.tar.gz ~/rpmbuild/SOURCES/
 
-    # Debian package info dir
-    mkdir -p $tmp_dir/DEBIAN
-    cp dist/linux/debian/control $tmp_dir/DEBIAN/
-    sed -i -- 's/%%PACKAGE_NAME%%/'$PACKAGE_NAME'/' $tmp_dir/DEBIAN/control
-    sed -i -- 's/%%PACKAGE_VERSION%%/'$PACKAGE_VERSION'/' $tmp_dir/DEBIAN/control
-    sed -i -- 's/%%MAINTAINER_NAME%%/'$MAINTAINER_NAME'/' $tmp_dir/DEBIAN/control
-    sed -i -- 's/%%MAINTAINER_CONTACT%%/'$MAINTAINER_CONTACT'/' $tmp_dir/DEBIAN/control
-    sed -i -- 's/%%GAME_DESCRIPTION%%/'"$GAME_DESCRIPTION"'/' $tmp_dir/DEBIAN/control
+    # Build and check the package
+    cd ~/rpmbuild/SPECS && rpmbuild -ba $spec_file
+    rpmlint $PACKAGE_NAME.rpm
 
+    exit 0
     # Documentation
     share_dir=$tmp_dir/usr/share
     doc_dir=$tmp_dir/usr/share/doc/$PACKAGE_NAME
@@ -77,19 +53,7 @@ function gen_rpm()
     cp dist/linux/debian/$PACKAGE_NAME.6 $section_dir/
     gzip -9 $section_dir/$PACKAGE_NAME.6
 
-    # Set the permissions
-    scripts/util/set_permissions.sh $tmp_dir
-    chmod 755 $exec_dir/$EXECUTABLE_NAME
-    chmod 755 $install_dir/$EXECUTABLE_NAME
-
-    # Strip executable debug symbols
-    strip $install_dir/$EXECUTABLE_NAME
-
-    # Build and check the package
-    fakeroot dpkg-deb --build $tmp_dir
-    mv /tmp/$OUTPUT_FILE .
-    lintian $OUTPUT_FILE
-}
+ }
 
 echo "Generating "$OUTPUT_FILE "..."
 gen_rpm
